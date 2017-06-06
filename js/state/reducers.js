c6bf3f7bd83517d82030886f9eecdcb76be189a2
Roman Shestakov <neoroma@gmail.com>
2017-06-06 23:35:13 +0300

    #8 Add time to the session part of the state

--- js/state/reducers.js
+++ js/state/reducers.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux'
 
 const DEFAULT_SESSION = {
-    day: 'MON'
+    day: 'MON',
+    time: new Date()
 }
 
 export const exerciseSession = (state = DEFAULT_SESSION, action) => {
     switch (action.type) {
         case 'PING':
             return {
-                day: state.day === 'MON' ? 'MONDAY' : 'MON'
+                day: state.day === 'MON' ? 'MONDAY' : 'MON',
+                time: new Date()
             }
         default:
             return state
