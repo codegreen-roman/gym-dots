9cd156f32689dacde1d6c61a700241083b79ea12
Oleg Rjumin <oleg.rjumin@gmail.com>
2017-06-08 00:01:22 +0300

    #11 Add sass support

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import { store } from './state/store'
 
 import { App } from './App'
+import './styles.scss'
 
 render(
     <Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>,
