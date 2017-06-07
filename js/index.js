fc5b9a9664ed5ae8b04a9a2fc6b85ef0f3b680b8
Oleg Rjumin <oleg.rjumin@gmail.com>
2017-06-07 23:08:49 +0300

    #10 Invoke configureStore function

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './state/store'
+import { configureStore } from './state/store'
 
 import { App } from './App'
+
+const store = configureStore()
 
 render(
     <Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>,
