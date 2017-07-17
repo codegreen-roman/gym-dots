94fcb4ff61b4212d825daabde717990522c32fc7
Roman Shestakov <neoroma@gmail.com>
2017-07-17 15:46:55 +0300

    #22 Add exercises obj to the initial state

--- js/state/reducers/initial-state.js
+++ js/state/reducers/initial-state.js
@@ -1,5 +1,35 @@
 export const INITIAL_STATE = {
     sessions: [],
+    workoutName: 'Monday',
     workoutStatus: 'notStarted',
-    exercises: []
+    exercises: {
+        upcoming: [
+            {
+                'restTime': 90,
+                'name': 'Push-ups',
+                'setsLeft': 5,
+                'reps': 20,
+                'weight': 0,
+                'results': []
+            },
+            {
+                'restTime': 60,
+                'name': 'Pull-up',
+                'setsLeft': 5,
+                'reps': 8,
+                'weight': 0,
+                'results': []
+            },
+            {
+                'restTime': 30,
+                'name': 'Australian pull-ups',
+                'setsLeft': 5,
+                'reps': 12,
+                'weight': 0,
+                'results': []
+            }
+        ],
+        skipped: [],
+        completed: [],
+    }
 }
