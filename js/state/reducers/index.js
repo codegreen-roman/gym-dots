173232bb5b5484f70cfda2692064691c07a315d1
Roman Shestakov <neoroma@gmail.com>
2017-07-17 17:00:23 +0300

    #22 Add reducer

--- js/state/reducers/index.js
+++ js/state/reducers/index.js
@@ -1,5 +1,6 @@
 import { exerciseSession } from './exerciseSession'
 import { githubLocation } from './testReducer'
 import { sessions } from './sessionsReducer'
+import { workoutStatus } from './workoutStatusReducer'
 
-export { exerciseSession, githubLocation, sessions }
+export { exerciseSession, githubLocation, sessions, workoutStatus }
