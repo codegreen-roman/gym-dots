cf1217525613cd3d3e920e9a713040fb935f48a3
Roman Shestakov <neoroma@gmail.com>
2017-07-21 13:09:02 +0300

    #42 Add reducer tests

---
+++ js/state/reducers/defaultsReducer.spec.js
@@ -0,0 +1,47 @@
+/* global expect, describe, beforeAll, beforeEach, fdescribe, it, xit */
+/* eslint-env jest */
+
+import { defaults } from './defaultsReducer'
+import { GOT_DEFAULTS } from '../actions/firebase/types'
+
+describe('Defaults reducer', () => {
+
+    const theAction = {
+        type: GOT_DEFAULTS,
+        payload: {
+            defaults: {
+                restTime: 100,
+                sets: 100
+            }
+        }
+    }
+
+    it('should return nice peace of state having restTime and sets as numbers', () => {
+        const state = defaults(undefined, theAction)
+        expect(state).toEqual(expect.objectContaining({
+            restTime: expect.any(Number),
+            sets: expect.any(Number),
+        }))
+    })
+
+    describe('with action not of this type', () => {
+
+        const initialState = {
+            restTime: 0,
+            sets: 0
+        }
+
+        const otherAction = {
+            type: 'OTHER',
+            payload: {}
+        }
+
+        it('should return the initial state', () => {
+            const state = defaults(initialState, otherAction)
+            expect(state).toEqual(expect.objectContaining({
+                restTime: 0,
+                sets: 0,
+            }))
+        })
+    })
+})
