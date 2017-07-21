58fed529b484eb21f0a6e29265b31ce3ba023ec9
Roman Shestakov <neoroma@gmail.com>
2017-07-21 12:40:03 +0300

    #42 Add showing sample defaults values form firebase in Activity

--- js/components/Activity.js
+++ js/components/Activity.js
@@ -1,24 +1,34 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { shape, string } from 'prop-types'
+import { shape, string, number } from 'prop-types'
 
-const _Activity = ({ exerciseSession: { day } }) => {
+const _Activity = ({ exerciseSession: { day }, defaults }) => {
 
     const now = +(new Date())
 
     return (
         <section style={{ border: '2px solid #9ACD32' }}>
             <div><span>{day}</span> rendered at {now}</div>
+
+            <pre>{JSON.stringify(defaults, null, 4)}</pre>
+
         </section>
     )
 }
 
 _Activity.propTypes = {
+    defaults: shape({
+        restTime: number,
+        sets: number
+    }).isRequired,
     exerciseSession: shape({
         day: string
-    })
+    }).isRequired
 }
 
-const mapStateToProps = ({ exerciseSession }) => ({ exerciseSession })
+const mapStateToProps = ({ exerciseSession, defaults }) => ({
+    exerciseSession,
+    defaults
+})
 
 export const Activity = connect(mapStateToProps)(_Activity)
