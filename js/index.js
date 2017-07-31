664f3751a5c660d0cf2f4656cce45cfccddc804b
Oleg Rjumin <orjumin@leongaming.com>
2017-08-01 00:42:44 +0300

    #39 Move app component to separate folder and add test

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { configureStore } from './state/store'
 
-import { App } from './components/App'
+import { App } from './components/app/App'
 
 const store = configureStore()
 
