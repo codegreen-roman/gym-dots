779ecafc6d661bd577203caf7325157952a18865
Roman Shestakov <neoroma@gmail.com>
2017-08-01 17:01:32 +0300

    Play with chunk plugin

--- server/start-server.js
+++ server/start-server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import compression from 'compression'
 
 export async function start(port) {
     // Create global app object
     const app = express()
 
     app.use(cors())
+    app.use(compression())
     app.use(express.static('build'))
 
     return new Promise(resolve => {
