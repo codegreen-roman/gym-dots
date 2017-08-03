847499eae8966f1e87bb2c345a647643953b8ae6
Roman Shestakov <neoroma@gmail.com>
2017-08-03 22:08:41 +0300

    #00 Add some comments and deps from the georgian eating place

--- js/components/views/activity/preWorkout/PreWorkout.js
+++ js/components/views/activity/preWorkout/PreWorkout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ManageExerciseList } from './manageExerciseList/ManageExerciseList'
 
+// pre exercise
 export const PreWorkout = () => {
     return (
         <section>
