c6277bc86b74cc9d9324611264b142b58e42deab
Roman Shestakov <neoroma@gmail.com>
2017-08-03 15:31:52 +0300

    #53 Add tests for PreWorkout holder

---
+++ js/components/views/activity/preWorkout/__tests__/PreWorkout.spec.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { PreWorkout } from '../PreWorkout'
+import { shallow } from 'enzyme'
+
+const setup = () => {
+
+    const props = {}
+
+    return {
+        component: shallow(<PreWorkout {...props} />),
+        props
+    }
+}
+
+describe('PreWorkout component', () => {
+    it('should contain one ManageExerciseList component', () => {
+        const { component } = setup()
+        expect(component.find('Connect(_ManageExerciseList)').length).toBe(1)
+    })
+
+    it('should match the snapshot', () => {
+        const { component } = setup()
+        expect(component).toMatchSnapshot()
+    })
+})
