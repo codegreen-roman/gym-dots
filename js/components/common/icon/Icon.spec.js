1a0b524b866c0164ca8dc6b59fcecbc2565e798d
Roman Shestakov <neoroma@gmail.com>
2017-08-04 10:17:45 +0300

    #00 Move glamor serializer

--- js/components/common/icon/Icon.spec.js
+++ js/components/common/icon/Icon.spec.js
@@ -1,15 +1,8 @@
-// TODO: Move to separate Jest config folder
-import { matcher, serializer } from 'jest-glamor-react'
-
-expect.extend(matcher)
-expect.addSnapshotSerializer(serializer)
-
 import React from 'react'
 import { render } from 'enzyme'
 import { Icon } from './Icon'
 
 describe('Icon Component', () => {
-
     it('renders with defaults', () => {
         const wrapper = render(<Icon icon='facebook' color='grey' />)
         expect(wrapper).toMatchSnapshotWithGlamor()
--- test-setup/setup.js
+++ test-setup/setup.js
@@ -1,2 +1,7 @@
+import { matcher, serializer } from 'jest-glamor-react'
+
+expect.extend(matcher)
+expect.addSnapshotSerializer(serializer)
+
 jest.mock('../js/state/actions/firebase/database')
 jest.mock('../js/components/common/header/UserImage.helper')
