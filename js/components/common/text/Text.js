36992bc69813e142b9d863f879a05d365a1e6aff
Oleg Rjumin <orjumin@leongaming.com>
2017-08-07 00:24:32 +0300

    #39 Add Text component tests

--- js/components/common/text/Text.js
+++ js/components/common/text/Text.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { Translate } from 'react-redux-i18n'
 import { string } from 'prop-types'
 
-export const Text = ({ text }) =>
-    <Translate value={text} />
+export const Text = ({ text }) => <Translate value={text} />
 
 Text.propTypes = {
     text: string.isRequired
