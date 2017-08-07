2833c3bf6e0779b2b55d95598bf7f23e7cc6dd6d
Oleg Rjumin <orjumin@leongaming.com>
2017-08-07 15:42:30 +0300

    #00 Update text component test

--- js/components/common/text/__tests__/Text.spec.js
+++ js/components/common/text/__tests__/Text.spec.js
@@ -1,45 +1,10 @@
 import React from 'react'
-import { render } from 'enzyme'
-import { Provider } from 'react-redux'
-import { configureStore } from '../../../../state/store'
-import { setLocale } from 'react-redux-i18n'
+import { shallow } from 'enzyme'
 import { Text } from '../Text'
 
 describe('Text component', () => {
-    let store, setup
-    beforeEach(() => {
-        store = configureStore()
-
-        setup = () => {
-            const wrapper = render(
-                <Provider store={store}>
-                    <Text text='greeting' />
-                </Provider>
-            )
-
-            return {
-                wrapper
-            }
-        }
-    })
-
-    it('should render defaults, which is `en` version of greeting', () => {
-        const { wrapper } = setup()
-        expect(wrapper.text()).toMatchSnapshot()
-        expect(wrapper.text()).toBe('Hello World!')
-    })
-
-    it('should render `ru` version of greeting', () => {
-        store.dispatch(setLocale('ru'))
-        const { wrapper } = setup()
-        expect(wrapper.text()).toMatchSnapshot()
-        expect(wrapper.text()).toBe('Привет мир!')
-    })
-
-    it('should render `ee` version of greeting', () => {
-        store.dispatch(setLocale('ee'))
-        const { wrapper } = setup()
-        expect(wrapper.text()).toMatchSnapshot()
-        expect(wrapper.text()).toBe('Tere maailm!')
+    it('should render defaults', () => {
+        const wrapper = shallow(<Text text='greeting' />)
+        expect(wrapper).toMatchSnapshot()
     })
 })
