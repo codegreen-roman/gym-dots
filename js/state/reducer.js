42e96d05aa53428eb7385612acc2d17edae8ec2e
Roman Shestakov <neoroma@gmail.com>
2017-08-21 17:07:33 +0300

    #57 Refactor reducer names in combine

--- js/state/reducer.js
+++ js/state/reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import { i18nReducer } from 'react-redux-i18n'
+import { i18nReducer as i18n } from 'react-redux-i18n'
 import { exerciseSession } from './reducers/exerciseSession.reducer'
 import { sessions } from './reducers/sessions.reducer'
 import { workoutStatus } from './reducers/workoutStatus.reducer'
@@ -9,12 +9,12 @@ import { exercises } from './reducers/exercises.reducer'
 import { currentExercise } from './reducers/currentExercise.reducer'
 
 export const reducer = combineReducers({
-    exercises: exercises,
+    exercises,
     exerciseSession,
     sessions,
     workoutStatus,
     defaults,
     auth,
-    i18n: i18nReducer,
+    i18n,
     currentExercise
 })
