b20a6c0763eb6f897d16f7e3268d520dc0c10d35
Roman Shestakov <neoroma@gmail.com>
2017-08-31 01:56:41 +0300

    #65 Add generateNext function to update next on results writes

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,23 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const R = require('ramda')
+
+const { allCompletedExercisesList } = require('./index.helper')
 
 admin.initializeApp(functions.config().firebase)
+
+const nextRef = admin.database().ref('/next')
+
+const transactionHandler = (error, committed, snapshot) => {
+    if (error) {
+        console.log('Transaction failed abnormally!', error)
+    } else if (!committed) {
+        console.log('We aborted the transaction')
+    } else {
+        console.log('weight is incremented')
+    }
+    console.log('new weight : ', snapshot.val())
+}
 
 exports.resultsForUser = functions.https.onRequest((req, res) => {
     const userKey = req.query.uid
@@ -12,3 +28,27 @@ exports.resultsForUser = functions.https.onRequest((req, res) => {
         .then(data => res.json(data))
         .catch(err => res.json(err))
 })
+
+exports.generateNext = functions.database.ref('/results/{userKey}/{pushId}/')
+    .onWrite(event => {
+
+        const incrementer = 5
+        const results = event.data.val()
+        const { userKey } = event.params
+
+
+        const toTransactionPromise = (key) => nextRef
+            .child(userKey)
+            .child('exercises')
+            .child(key)
+            .child('weight')
+            .transaction(
+                R.add(incrementer),
+                transactionHandler
+            )
+
+        const getPromisesFrom = R.compose(R.map(toTransactionPromise), allCompletedExercisesList)
+
+        return Promise.all(getPromisesFrom(results))
+
+    })
