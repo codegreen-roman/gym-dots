16e1f16d7d208f7acbb2d062f3b8e5903c0c057c
Roman Shestakov <neoroma@gmail.com>
2017-10-10 23:56:38 +0300

    #74 Add some ramda magic

--- js/state/middleware/database/firebaseApi.js
+++ js/state/middleware/database/firebaseApi.js
@@ -3,9 +3,15 @@
 
 import { types } from '../../'
 
-import R from 'ramda'
+import R, { prop, compose, equals } from 'ramda'
 import { logout, loginWith, loginAnonymously } from './database'
 import { subscribeToAuthStateChanged } from './actions'
+
+export const getType = prop('type')
+
+export const typeOfAuthProvider = compose(equals('FBASE:AUTH_PROVIDER'), getType)
+export const typeOfAuthGuest = compose(equals('FBASE:AUTH_GUEST'), getType)
+export const typeOfAuthVoid = compose(equals('FBASE:AUTH_VOID_START'), getType)
 
 export function firebase(store) {
 
@@ -13,22 +19,20 @@ export function firebase(store) {
 
     subscribeToAuthStateChanged(dispatch)
 
-    return (next) => ({ type, payload }) => {
+    return (next) => ({ type, payload = {} }) => {
+
+        const { provider } = payload
+        const loginForProvider = (provider) => () => loginWith(provider)
 
         next({ type, payload })
 
-        if (R.startsWith('FBASE:')(type)) {
-            if (type === 'FBASE:AUTH_PROVIDER') {
-                loginWith(payload.provider)
-            }
+        const app = R.cond([
+            [typeOfAuthProvider, loginForProvider(provider)],
+            [typeOfAuthGuest, loginAnonymously],
+            [typeOfAuthVoid, logout],
+            [R.T, () => {}]
+        ])
 
-            if (type === 'FBASE:AUTH_GUEST') {
-                loginAnonymously()
-            }
-
-            if (type === 'FBASE:AUTH_VOID_START') {
-                logout()
-            }
-        }
+        app({ type, payload })
     }
 }
