7f3b9bb7a8e2782c064111b621723b51b8ed73b7
Roman Shestakov <neoroma@gmail.com>
2017-10-13 15:26:11 +0300

    #00 Add the ErrorBoundary example component

--- js/components/views/activity/preWorkout/PreWorkout.js
+++ js/components/views/activity/preWorkout/PreWorkout.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { ErrorBoundary } from '../../../common/error-boundary/ErrorBoundary'
 import { ManageExerciseList } from './manageExerciseList/ManageExerciseList.connect'
 
 // pre exercise
 export const PreWorkout = () => {
     return (
-        <section>
+        <ErrorBoundary>
             <ManageExerciseList />
-        </section>
+        </ErrorBoundary>
     )
 }
