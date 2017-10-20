73df5e1f84c8e43fabeb967670ce7ab4f246ccb4
Oleg Rjumin <orjumin@leongaming.com>
2017-10-20 17:52:06 +0300

    #00 Refactor ‘Footer’ component
    - Add FooterLayoutHOC comp
    - FooterButtons comp
    - Clear Footer render method

--- src/components/common/footer/Footer.js
+++ src/components/common/footer/Footer.js
@@ -3,19 +3,56 @@ import { FooterButton } from './buttons/FooterButton'
 import { footerStyle } from './Footer.glamor'
 import { func, bool, object, array } from 'prop-types'
 import { Flex } from 'glamorous-jsxstyle'
+import { branch, RenderNothing } from '@utils/helpers'
+
+
+const FooterLayoutHOC = (Component) => {
+    const Layout = props => (
+        <footer {...footerStyle}>
+            <Flex>
+                <Component {...props} />
+            </Flex>
+        </footer>
+    )
+    return Layout
+}
+
+
+const _FooterButtons = ({ fireStartWorkout, blocked, training, onSetFailed, onSetDone, nextExercise }) => {
+
+    const startButtonHandler = () => fireStartWorkout(nextExercise)
+
+    return branch(
+        training,
+        (
+            <div>
+                <FooterButton key={1} clickHandler={onSetFailed} data-test='failButton' disabled={false}>Failed</FooterButton>
+                <FooterButton key={2} clickHandler={onSetDone} data-test='doneButton' disabled={false}>Done</FooterButton>
+            </div>
+        ),
+        <FooterButton disabled={blocked} clickHandler={startButtonHandler}>{blocked ? 'Starting ...' : 'Start Workout'}</FooterButton>
+    )
+}
+
+_FooterButtons.propTypes = {
+    fireStartWorkout: func.isRequired,
+    blocked: bool.isRequired,
+    training: bool.isRequired,
+    onSetFailed: func.isRequired,
+    onSetDone: func.isRequired,
+    nextExercise: object
+}
+
+export const FooterButtons = FooterLayoutHOC(_FooterButtons)
+
 
 export class Footer extends React.Component {
     static propTypes = {
-        blocked: bool.isRequired,
-        training: bool.isRequired,
-        hidden: bool.isRequired,
-        fireStartWorkout: func.isRequired,
-        fireCompleteExercise: func.isRequired,
-        onSetFailed: func.isRequired,
-        onSetDone: func.isRequired,
         shouldEndExercise: bool.isRequired,
+        fireCompleteExercise: func.isRequired,
         nextExercise: object,
-        currentResults: array.isRequired
+        currentResults: array.isRequired,
+        hidden: bool.isRequired
     }
 
     componentWillReceiveProps({ shouldEndExercise, fireCompleteExercise, nextExercise: { exerciseKey }, currentResults }) {
@@ -24,41 +61,11 @@ export class Footer extends React.Component {
         }
     }
 
-    renderTrainingButtons() {
-
-        const { fireStartWorkout, blocked, training, onSetFailed, onSetDone, nextExercise } = this.props
-        const buttonTitle = blocked ? 'Starting ...' : 'Start Workout'
-
-        const startButtonHandler = () => {
-            fireStartWorkout(nextExercise)
-        }
-
-        if (training) {
-            return (
-                <Flex>
-                    <FooterButton clickHandler={onSetFailed} data-test='failButton' disabled={false}>Failed</FooterButton>
-                    <FooterButton clickHandler={onSetDone} data-test='doneButton' disabled={false}>Done</FooterButton>
-                </Flex>
-            )
-        }
-
-        return (
-            <Flex>
-                <FooterButton disabled={blocked} clickHandler={startButtonHandler}>{buttonTitle}</FooterButton>
-            </Flex>
-        )
-    }
-
     render() {
-
-        const { hidden } = this.props
-
-        if (hidden) return null
-
-        return (
-            <footer {...footerStyle}>
-                {this.renderTrainingButtons()}
-            </footer>
+        return branch(
+            this.props.hidden,
+            <RenderNothing />,
+            <FooterButtons {...this.props} />
         )
     }
 }
