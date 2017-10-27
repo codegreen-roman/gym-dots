7ced8b813e93d37d157530735012ec03c167955c
Oleg Rjumin <orjumin@leongaming.com>
2017-10-27 20:15:29 +0300

    #00 Add extractTargetValue helper test

--- src/utils/__tests__/helpers.spec.js
+++ src/utils/__tests__/helpers.spec.js
@@ -1,4 +1,8 @@
-import { notEmpty, mapIndexed } from '@utils/helpers'
+import {
+    notEmpty,
+    mapIndexed,
+    extractTargetValue
+} from '@utils/helpers'
 
 test('.notEmpty works as expected', () => {
     expect(typeof notEmpty === 'function').toBeTruthy()
@@ -13,3 +17,14 @@ test('.mapIndexed adds indexes to map', () => {
     expect(typeof mapIndexed === 'function').toBeTruthy()
     expect(mapIndexed((item, idx) => item + idx, ['a','b','c'])).toEqual(['a0','b1','c2'])
 })
+
+test('.extractTargetValue extracts value properly', () => {
+    const event = {
+        target: {
+            value: 'Default'
+        }
+    }
+    expect(typeof extractTargetValue === 'function').toBeTruthy()
+    expect(extractTargetValue(event)).toEqual('Default')
+})
+
