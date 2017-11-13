bcfe17444b596a5f485bd9f8618cd653c8ee7012
Oleg Rjumin <orjumin@leongaming.com>
2017-11-13 22:41:30 +0200

    #87 Add bootstrap-reboot.css and inject some global styles

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import { ConnectedRouter as Router } from 'react-router-redux'
 import { Provider } from 'react-redux'
 import { configureStore } from './state/store'
 import { Main } from './components/main/Main'
-import 'normalize.css'
-import './utils/reset.css'
+import { css } from 'glamor'
+import './utils/bootstrap-reboot.css'
+
+/* Global styles */
+css.global('html, body', { height: '100%' })
+css.global('#app', { height: '100%', fontFamily: 'Nunito' })
 
 const { store, history } = configureStore()
 
