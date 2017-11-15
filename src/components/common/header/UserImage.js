3a717c79f3e9657da19fb71662429d4662adb528
Oleg Rjumin <orjumin@leongaming.com>
2017-11-15 22:46:09 +0200

    #87 Add functional way of rendering ‘UserImage’ depending on diff conditions
    - Add WithUserNameHOC to be able to wrap image with username, or any other component
    - Add small components that could be used elsewhere
    - Forgot to implement tests, fuck. Will do next time

--- src/components/common/header/UserImage.js
+++ src/components/common/header/UserImage.js
@@ -1,24 +1,51 @@
 import React from 'react'
 import glamorous from 'glamorous'
+import { userPicStyles, userNameStyles } from './UserImage.glamor'
 import { string } from 'prop-types'
-import { branch, RenderNothing } from '@utils/helpers'
+import { always, cond, T } from 'ramda'
+import { isNotNil } from '../../../utils/helpers'
+import { Aux } from '../../../utils/enhancers'
 
-const ImgForUser = glamorous.img({
-    width: '3rem',
-    borderRadius: '2rem'
-})
+/* UserPic: holding user image */
+export const UserPic = ({ image, ...other }) =>
+    <StyledUserImage {...other} src={image} alt='This is your avatar' />
 
-export const UserImage = ({ image }) =>
-    branch(
-        image,
-        <ImgForUser src={image} alt='This is your avatar' />,
-        <RenderNothing />
+UserPic.propTypes = { image: string }
+const StyledUserImage = glamorous.img(userPicStyles)
+
+/* UserName: holding username */
+export const UserName = ({ userDisplayName, ...other }) =>
+    <StyledUserName {...other}>{userDisplayName} </StyledUserName>
+
+UserName.propTypes = { userDisplayName: string }
+const StyledUserName = glamorous.div(userNameStyles)
+
+/* WithUserNameHOC: adds username below image */
+const WithUserNameHOC = (Component) => {
+    const WithUserName = props => (
+        <Aux>
+            <Component {...props} />
+            <UserName
+                data-test='username'
+                userDisplayName={props.userDisplayName}
+            />
+        </Aux>
     )
-
-UserImage.propTypes = {
-    image: string
+    WithUserName.propTypes = { userDisplayName: string }
+    return WithUserName
 }
 
-UserImage.defaultProps = {
-    image: ''
-}
+export const WithUserName = WithUserNameHOC(UserPic)
+
+/* Conditions */
+const hasImage = (props) => isNotNil(props.image)
+const hasUserNameAndImage = (props) => isNotNil(props.userDisplayName) && isNotNil(props.image)
+
+/* Conditional render */
+export const conditionalRender = (props) => cond([
+    [hasUserNameAndImage, always(<WithUserName key='with-username' {...props} />)],
+    [hasImage, always(<UserPic key='user-pic' image={props.image} />)],
+    [T, always(null)]
+])(props)
+
+export const UserImage = (props) => <Aux>{conditionalRender(props)}</Aux>
