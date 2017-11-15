83ca902d9f8a059ff7a43c5bf1378f1a468fa3ac
Oleg Rjumin <orjumin@leongaming.com>
2017-11-15 22:31:42 +0200

    #87 Refactor ‘Arc’ test, make use of main setup function

--- src/components/common/circle/__tests__/Arc.spec.js
+++ src/components/common/circle/__tests__/Arc.spec.js
@@ -2,41 +2,38 @@ import React from 'react'
 import { render } from 'enzyme'
 import { Arc } from '../Arc'
 
-const defaultProps = {
-    d: 'M 205.1056516 79.0983006 A 100 100 0 0 0 127.3648178 11.5192247',
-    arcClass: 'toBeDone'
-}
 
-const setup = (arcClass=defaultProps.arcClass, d = defaultProps.d) => {
-    return render(<Arc d={d} arcClass={arcClass} />)
+const setup = (updateProps = {}) => {
+    const defaultProps = {
+        d: 'M 205.1056516 79.0983006 A 100 100 0 0 0 127.3648178 11.5192247',
+        arcClass: 'toBeDone',
+        ...updateProps
+    }
+
+    return render(<Arc {...defaultProps} />)
 }
 
 describe('Arc Component', () => {
-    let wrapper
-    let path
-
-    beforeEach(() => {
-        wrapper = setup()
-        path = 'M 110 10 A 100 100 0 0 0 21.7052407 63.0528437'
-    })
-
     test('should render correct color by default', () => {
+        const wrapper = setup()
         expect(wrapper).toMatchSnapshotWithGlamor()
     })
 
     test('should render correct color when completed', () => {
-        wrapper = setup('completed')
+        const wrapper = setup({arcClass: 'completed'})
         expect(wrapper).toMatchSnapshotWithGlamor()
     })
 
     test('should render correct color when failed', () => {
-        wrapper = setup('failed')
+        const wrapper = setup({arcClass: 'failed'})
         expect(wrapper).toMatchSnapshotWithGlamor()
     })
 
     test('should apply path when provided', () => {
-        wrapper = setup('toBeDone', 'M 110 10 A 100 100 0 0 0 21.7052407 63.0528437')
+        const wrapper = setup({
+            arcClass: 'toBeDone',
+            d: 'M 110 10 A 100 100 0 0 0 21.7052407 63.0528437'
+        })
         expect(wrapper).toMatchSnapshot()
     })
-
 })
